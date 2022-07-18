29e091edbbee69861bfcd91e0e490e05257ad064
Matthew Hill <101017377+mattdhill011@users.noreply.github.com>
2022-07-18 00:26:53 -0400

    mostly finished part 1

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,23 +1,57 @@
-// Create the tile layer that will be the background of our map.
-var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+url = "https://raw.githubusercontent.com/mattdhill011/leaflet-challenge/main/Leaflet-Step-1/all_month.geojson"
+
+
+d3.json(url).then(function(earthquakes) {
+
+    // Create the tile layer that will be the background of our map.
+    var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+    var map = L.map("map", {
+        center: [0,0],
+        zoom: 3,
+        layers: [streetmap]
+
+    });
+
+    for (var i =0; i < earthquakes.features.length; i++) {
+
+        var color = "";
+
+        var coord = [earthquakes.features[i].geometry.coordinates[1], earthquakes.features[i].geometry.coordinates[0]]
+        var mag = earthquakes.features[i].properties.mag
+        var depth = earthquakes.features[i].geometry.coordinates[2]
+        var date = new Date(earthquakes.features[i].properties.time)
+
+        if (depth <= 10) {
+            color = '#fef0d9';
+        } else if (depth <= 30) {
+            color = '#fdd49e';
+        } else if (depth <= 50) {
+            color = '#fdbb84';
+        } else if (depth <= 70) {
+            color = '#fc8d59';
+        } else if (depth <=90) {
+            color = '#e34a33';
+        } else {
+            color = '#b30000';
+        };
+
+        L.circle(coord, {
+            fillOpacity: 0.75,
+            color: "black",
+            fillColor: color,
+            radius: mag * 20000
+        }).bindPopup(`
+            <h2>${earthquakes.features[i].properties.place}</h2>
+            <p>Magnitude: ${mag}<br>
+            Depth: ${depth} km<br>
+            Time Recorded: ${date}</p>
+        `).addTo(map);
+    }
+
+
 });
 
-// Initialize all the LayerGroups that we'll use.
-var layers = {
-    earthquakeLayer: new L.LayerGroup()
-};
 
-var map = L.map("map", {
-    center: [0,0],
-    zoom: 3,
-    layers: [
-        layers.earthquakeLayer
-    ]
-});
-
-streetmap.addTo(map)
-
-var overlays = {
-    "Earthquakes": layers.earthquakeLayer
-}
