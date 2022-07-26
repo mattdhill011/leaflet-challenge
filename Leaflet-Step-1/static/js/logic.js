66528945047f31bab023435e31957c2db52c9d96
Matthew Hill <101017377+mattdhill011@users.noreply.github.com>
2022-07-26 12:54:31 -0400

    finished

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -3,17 +3,7 @@ plateUrl = "https://raw.githubusercontent.com/mattdhill011/leaflet-challenge/mai
 
 d3.json(url).then(function(earthquakes) {
 
-    // Create the tile layer that will be the background of our map.
-    var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    });
-
-    var map = L.map("map", {
-        center: [0,0],
-        zoom: 3,
-        layers: [streetmap]
-
-    });
+    var earthquakeLayer = new L.layerGroup();
 
     for (var i =0; i < earthquakes.features.length; i++) {
 
@@ -25,18 +15,19 @@ d3.json(url).then(function(earthquakes) {
         var date = new Date(earthquakes.features[i].properties.time)
 
         if (depth <= 10) {
-            color = '#fef0d9';
+            color = '#2aa10f';
         } else if (depth <= 30) {
-            color = '#fdd49e';
+            color = '#92e000';
         } else if (depth <= 50) {
-            color = '#fdbb84';
+            color = '#e1ff00';
         } else if (depth <= 70) {
-            color = '#fc8d59';
+            color = '#f58b00';
         } else if (depth <=90) {
-            color = '#e34a33';
+            color = '#de3700';
         } else {
             color = '#b30000';
         };
+
 
         L.circle(coord, {
             fillOpacity: 0.75,
@@ -48,23 +39,86 @@ d3.json(url).then(function(earthquakes) {
             <p>Magnitude: ${mag}<br>
             Depth: ${depth} km<br>
             Time Recorded: ${date}</p>
-        `).addTo(map);
+        `).addTo(earthquakeLayer)
     }
 
+    var plateBoundryLayer = new L.layerGroup();
+
     d3.json(plateUrl).then(function(plateBoundry) {
+
+        function reverseCoords(coords) {
+            return [coords[1], coords[0]];
+        };
         for (var i = 0; i < plateBoundry.features.length; i++) {
             var plateCoord = plateBoundry.features[i].geometry.coordinates;
-            console.log(plateCoord);
             var reversedCoord = [];
-            for (var j = 0; j < plateCoord.length; i++) {
-                reversedCoord.push([plateCoord[j][1], plateCoord[j][0]]);
+
+            for (var j = 0; j < plateCoord.length; j++) {
+                reversedCoord.push(reverseCoords(plateCoord[j]));
             };
-            L.polygon(reversedCoord, {
-                color:"black"
-            }).addTo(map);
-        }
+
+            L.polyline(reversedCoord).addTo(plateBoundryLayer);
+        };
+    });
+    
+    // Create the tile layer that will be the background of our map.
+    var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
 
+    var googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+	subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    });
+
+    var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    });
+
+    var baseMaps = {
+        "OpenStreetMap": streetmap,
+        "Google satellite": googleSat,
+        "Topographic":topo
+    };
+
+    var overlayMaps = {
+        "Earthquakes": earthquakeLayer,
+        "Plate Boundries": plateBoundryLayer
+    };   
+
+    var map = L.map("map", {
+        center: [0,0],
+        zoom: 3,
+        layers: [streetmap, googleSat, topo, earthquakeLayer, plateBoundryLayer]
+
+    });
+
+    function getColor(m) {
+        return m <= 10 ? '#2aa10f':
+        m <= 30 ? '#92e000':
+        m <= 50 ? '#e1ff00':
+        m <= 70 ? '#f58b00':
+        m <=90 ? '#de3700':
+        '#b30000';
+}
+
+    var legend = L.control({position: 'bottomright'});
+    legend.onAdd = function (map) {
+        var div = L.DomUtil.create('div', 'info legend'),
+        grades = ['-10', '10', '30', '50', '70', '90'];
+
+        div.innerHTML += "Earthquake Depth" + '<br>'
+        
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(grades[i]) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+        return div;
+    };
+
+    legend.addTo(map);
+
+    var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
 
 });
 
